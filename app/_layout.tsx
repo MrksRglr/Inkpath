21f5d9ee4a6f9550e233e31b847ce31e945678ae
Mr Happy <s2210456031@fhooe.at>
2024-07-07 00:56:04 +0200

    initial + update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,18 +5,12 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
-import { useColorScheme } from '@/components/useColorScheme';
+import {useColorScheme} from "react-native";
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from 'expo-router';
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
---
+++ app/list.tsx
@@ -0,0 +1,9 @@
+import {View,Text} from "react-native";
+
+export default function List() {
+  return (
+    <View>
+      <Text>list</Text>
+    </View>
+  );
+}
